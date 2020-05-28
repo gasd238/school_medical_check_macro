35ef2e851409e147b836a3bb1fb2e49379dbc703
Bluerain <41102800+gasd238@users.noreply.github.com>
2020-05-28 21:13:56 +0900

    update

--- 건강체크 매크로/test.py
+++ 건강체크 매크로/test.py
@@ -1,7 +1,11 @@
+#-*- coding:utf-8 -*-
+
 from selenium import webdriver
 import time
 
-
+a = input("이름 생년월일을 홍길동,123456 이런 식으로 넣어주세요 : ")
+name = a.split(',')[0]
+day = a.split(',')[1]
 
 chrdir = "./chromedriver.exe"
 driver = webdriver.Chrome(chrdir)
@@ -11,8 +15,8 @@ driver.find_element_by_xpath('//*[@id="schulNm"]').click()
 time.sleep(3)
 driver.find_element_by_name("schulNm").send_keys("소프트") 
 driver.find_element_by_xpath('//*[@id="btnSrchSchul"]').click()
-driver.find_element_by_name("pName").send_keys("") #자기 이름
-driver.find_element_by_name("frnoRidno").send_keys("") # 생년월일
+driver.find_element_by_name("pName").send_keys(name) #자기 이름
+driver.find_element_by_name("frnoRidno").send_keys(day) # 생년월일
 driver.find_element_by_xpath('//*[@id="btnConfirm"]').click()
 time.sleep(2)
 driver.find_element_by_xpath('//*[@id="infoForm"]/div/div/table[1]/tbody/tr[2]/td/span/label[1]').click()
@@ -20,6 +24,7 @@ driver.find_element_by_xpath('//*[@id="infoForm"]/div/div/table[2]/tbody/tr[2]/t
 driver.find_element_by_xpath('//*[@id="infoForm"]/div/div/table[3]/tbody/tr[2]/td/span/label[1]').click()
 driver.find_element_by_xpath('//*[@id="infoForm"]/div/div/table[4]/tbody/tr[2]/td/span/label[1]').click()
 driver.find_element_by_xpath('//*[@id="infoForm"]/div/div/table[5]/tbody/tr[2]/td/span/label[1]').click()
+time.sleep(5)
 driver.find_element_by_xpath('//*[@id="btnConfirm"]').click()
 time.sleep(3)
 driver.quit()
