8dab14dfba5581a4c8907de8a2a3f52e73a46890
Bluerain <41102800+gasd238@users.noreply.github.com>
2020-05-28 20:48:00 +0900

    Add files via upload

---
+++ 건강체크 매크로/test.py
@@ -0,0 +1,28 @@
+from bs4 import BeautifulSoup
+import requests
+import lxml
+from selenium import webdriver
+import time
+
+
+
+chrdir = "./chromedriver.exe"
+driver = webdriver.Chrome(chrdir)
+driver.get("https://eduro.gen.go.kr/hcheck/index.jsp")
+driver.find_element_by_xpath('//*[@id="container"]/div/div/div/div[2]/div/a[2]/div').click()
+driver.find_element_by_xpath('//*[@id="schulNm"]').click()
+time.sleep(3)
+driver.find_element_by_name("schulNm").send_keys("소프트") 
+driver.find_element_by_xpath('//*[@id="btnSrchSchul"]').click()
+driver.find_element_by_name("pName").send_keys("박재홍") #자기 이름
+driver.find_element_by_name("frnoRidno").send_keys("021018") # 생년월일
+driver.find_element_by_xpath('//*[@id="btnConfirm"]').click()
+time.sleep(2)
+driver.find_element_by_xpath('//*[@id="infoForm"]/div/div/table[1]/tbody/tr[2]/td/span/label[1]').click()
+driver.find_element_by_xpath('//*[@id="infoForm"]/div/div/table[2]/tbody/tr[2]/td/span/label[1]').click()
+driver.find_element_by_xpath('//*[@id="infoForm"]/div/div/table[3]/tbody/tr[2]/td/span/label[1]').click()
+driver.find_element_by_xpath('//*[@id="infoForm"]/div/div/table[4]/tbody/tr[2]/td/span/label[1]').click()
+driver.find_element_by_xpath('//*[@id="infoForm"]/div/div/table[5]/tbody/tr[2]/td/span/label[1]').click()
+driver.find_element_by_xpath('//*[@id="btnConfirm"]').click()
+time.sleep(3)
+driver.quit()
